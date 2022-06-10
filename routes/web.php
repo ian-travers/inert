dec4f0b49b0ceec63df0cb89dc5ec66672f8ace8
Ian Travers <mr.itpe@gmail.com>
2022-06-10 09:36:41 +0300

    Ep.04 Inertia Links

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,13 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Hello', [
-        'name' => 'Jeffrey Way',
-        'frameworks' => ['Laravel', 'Inertia', 'Vue'],
-    ]);
+    return Inertia::render('Hello');
+});
+
+Route::get('/users', function () {
+    return Inertia::render('Users');
+});
+
+Route::get('/settings', function () {
+    return Inertia::render('Settings');
 });
