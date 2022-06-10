7933df7f54e9e9091c38fd10e28f7f25caaf7466
Ian Travers <mr.itpe@gmail.com>
2022-06-10 11:15:37 +0300

    Ep.08 Active Links

---
+++ resources/js/Shared/NavLink.vue
@@ -0,0 +1,16 @@
+<template>
+  <Link
+    class="text-blue-500 hover:underline"
+    :class="{'font-bold underline': active}"
+  >
+    <slot/>
+  </Link>
+</template>
+
+<script setup>
+import { Link } from "@inertiajs/inertia-vue3"
+
+defineProps({
+  active: Boolean
+})
+</script>
