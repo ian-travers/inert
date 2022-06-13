aa84a591c5cbc13c603f4daf779856143b328b35
Ian Travers <mr.itpe@gmail.com>
2022-06-13 11:42:20 +0300

    Move Scroll example to the setting page

--- resources/js/Pages/Settings.vue
+++ resources/js/Pages/Settings.vue
@@ -9,4 +9,15 @@
   </Head>
 
   <h1 class="text-3xl">Settings</h1>
+
+  <div style="margin-top: 700px">
+    <p class="text-lg">The current time is {{ time }}.</p>
+    <Link class="text-blue-500 hover:underline" href="/settings" preserve-scroll>Refresh</Link>
+  </div>
 </template>
+
+<script setup>
+defineProps({
+  time: String,
+})
+</script>
--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -9,15 +9,4 @@
   </Head>
 
   <h1 class="text-3xl">Users</h1>
-
-  <div style="margin-top: 700px">
-    <p class="text-lg">The current time is {{ time }}.</p>
-    <Link class="text-blue-500 hover:underline" href="/users" preserve-scroll>Refresh</Link>
-  </div>
 </template>
-
-<script setup>
-defineProps({
-  time: String,
-})
-</script>
