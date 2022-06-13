1b2052d3e3b851397393bdff1494997135773b54
Ian Travers <mr.itpe@gmail.com>
2022-06-13 11:35:57 +0300

    SSR Head Description Meta tag

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
+    <meta name="description" content="{{ $description ?? 'My app information' }}">
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet"/>
     <script src="{{ mix('/js/manifest.js') }}" defer></script>
     <script src="{{ mix('/js/vendor.js') }}" defer></script>
--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,20 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Home');
+    return Inertia::render('Home')
+        ->withViewData(['description' => 'My App description']);
 });
 
 Route::get('/users', function () {
     return Inertia::render('Users', [
         'time' => now()->toTimeString(),
-    ]);
+    ])
+        ->withViewData(['description' => 'Users page description']);
 });
 
 Route::get('/settings', function () {
-    return Inertia::render('Settings');
+    return Inertia::render('Settings')
+        ->withViewData(['description' => 'Settings page description']);
 });
 
 Route::post('/logout', function () {
